f70f0cd14a87cf90ca6cccf5fdf9fc5a04db2c94
Jessica Zarate <113988178+JessicaCZarate@users.noreply.github.com>
2026-03-06 02:12:42 +0800

    added all data

---
+++ src/data/clientInfo.ts
@@ -0,0 +1,102 @@
+export const clientInfo = {
+  couple: {
+    groom: {
+      fullName: "Jerone Francisco San Roque",
+      preferredName: "Ron",
+    },
+    bride: {
+      fullName: "Mikkie Sarmenta Gueco",
+      preferredName: "Mikkie",
+    },
+  },
+  parents: {
+    groom: [
+      "Jordan San Roque",
+      "Concepcion Francisco San Roque",
+    ],
+    bride: [
+      "Rodolfo Gueco",
+      "Cristina Sarmenta Gueco",
+    ],
+  },
+  eventDetails: {
+    date: "May 22, 2026",
+    time: "2:00 PM",
+    church: {
+      name: "Lokal Ng Lingunan Distrito ng CAMANAVA",
+      address: "Lokal Ng Lingunan Distrito ng CAMANAVA",
+    },
+    reception: {
+      name: "Casa De Polo",
+      address: "Valenzuela City",
+    },
+  },
+  principalSponsors: [
+    {
+      name: "Nelson Ramirez",
+      partneredWith: "Marinette Bote",
+    },
+    {
+      name: "Ferdinand Guererro",
+      partneredWith: "Irene Guererro",
+    },
+    {
+      name: "Ferdinand Bataan",
+      partneredWith: "Jhoanalyn Figueroa",
+    },
+    {
+      name: "Danilo Mondero",
+      partneredWith: "Mervelous Estole",
+    },
+    {
+      name: "Alejandro Gregorio",
+      partneredWith: "Cecilia Trinidad",
+    },
+  ],
+  wedding_party: {
+    bestMan: "Jericone F. San Roque",
+    maidOfHonor: "Cassandra S. Aquino",
+    groomsmen: [
+      "Dr. Eirom Nicole B. Tanaka",
+      "Robert Morados",
+      "Vincent H. Santiago",
+      "Ernesto C. Supan Jr",
+    ],
+    bridesmaids: [
+      "Rubelyn G. Aquino",
+      "Robelle Aira Aguado",
+      "Jewel Kerubine Pascual-Imperial",
+      "Rydeen Viray",
+    ],
+    flowerGirls: [
+      "Jenoah Nyxie Gueco Lachica",
+      "Alicia Coleen Francisco",
+    ],
+  },
+  rsvp: {
+    deadline: "On or Before May 05, 2026",
+    cutoffDate: "On or Before May 12, 2026",
+    contactPerson: "Ron",
+    contactNumber: "0966 148 4903",
+  },
+  attireAndColorPalette: {
+    ninong: {
+      role: "Ninong (Principal Sponsors - Male)",
+      attire: "Barong Tagalog, black pants and formal shoes",
+    },
+    ninang: {
+      role: "Ninang (Principal Sponsors - Female)",
+      attire: "Modern / Filipiniana dress nude/beige",
+    },
+    guestWomen: {
+      role: "Guest Women",
+      attire: "Modern Filipiniana Dress or Formal Dress and sandals or heels",
+      restrictions: "No maong, not sleeveless spaghetti dress or cocktail dress",
+    },
+    guestMale: {
+      role: "Guest Male",
+      attire: "Modern polo or Formal Attire and formal shoes",
+      restrictions: "No maong pants, slippers, sando",
+    },
+  },
+};
